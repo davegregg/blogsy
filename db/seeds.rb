3ba87113dd2ec7c52e978155c56a721a1a387855
davegregg <davegregg@gmail.com>
2017-03-01 09:32:32 -0500

    To make the seed simulate reality better, I now have users' comments generating randomly among all existing posts, instead of only commenting on their own posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,22 @@
   generate_user = -> { User.new(username: FFaker::InternetSE.user_name_random,
                                 email: FFaker::Internet.email,
                                 bio: FFaker::CheesyLingo.sentence) }
+  generate_comment = -> (user){ Comment.new(body: FFaker::Tweet.tweet,
+                                            user: user) }
   user = generate_user.()
   rand(1..31).times do
     post = Post.new(title: FFaker::Movie.title,
                     body: FFaker::DizzleIpsum.paragraph)
     commenter = generate_user.()
-    rand(7).times do
-      comment = Comment.new(body: FFaker::Tweet.tweet,
-                            user: commenter)
-      post.comments << comment
+    rand(4).times do
+      post.comments << generate_comment.(commenter)
     end
     user.posts << post
     user.save!
+  end
+  rand(4).times do
+    post = Post.all[rand(Post.all.count)]
+    post.comments << generate_comment.(user)
+    post.save!
   end
 end
